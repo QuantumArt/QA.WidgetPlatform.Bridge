c96f167933fcfe69562273540090f03027085dba
ARTQ\alferovv <AlferovV@quantumart.ru>
2024-03-05 12:44:20 +0300

    8.0.11

---
+++ src/components/on-screen-article/on-screen-article.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export interface IOnScreenArticleComponentProps {
+  title: string;
+  contentId: number;
+  published: boolean;
+  children: React.ReactNode;
+}
+
+type OnScreenArticleComponent = (props: IOnScreenArticleComponentProps) => JSX.Element;
+export const OnScreenArticleContext = React.createContext<OnScreenArticleComponent>(undefined);
+export const useOnScreenArticle = (): OnScreenArticleComponent =>
+  React.useContext(OnScreenArticleContext)!;
+
+export function OnScreenArticle(props: IOnScreenArticleComponentProps): JSX.Element {
+  const store = useOnScreenArticle();
+  return store(props);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 /** components **/
 export { default as WidgetZone } from './components/widget-zone/widget-zone';
+export * from './components/on-screen-article/on-screen-article';
 
 /** stores **/
 export * from './stores/abstract-item/abstract-item';
 export * from './stores/zone/zone-store';
 export * from './stores/wp-graphql-client/wp-graphql-client';
-export * from './stores/wp-routes/wp-routes-store'
-export * from './stores/event-bus/event-bus-store'
+export * from './stores/wp-routes/wp-routes-store';
+export * from './stores/event-bus/event-bus-store';
